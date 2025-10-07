375b3e3e22c89964112ac7a5b1650745b90e8ee1
shanmengchunyu <2310495783@qq.com>
2025-10-07 08:37:32 +0800

    使用ViT模型进行训练，并且新增了显示一些超参数的功能

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 
 import torch
 import torch.optim as optim
@@ -10,6 +11,8 @@ from torch.utils.tensorboard import SummaryWriter
 from torch.cuda.amp import GradScaler, autocast
 
 import torchvision.models as models
+from torchvision.models import ViT_B_16_Weights
+
 from data_process import ImagePreprocessor
 from data_process import ImageAugmentor
 from dataset import Dataset
@@ -30,14 +33,20 @@ def create_unique_dir(path):
             i += 1
 
 def main(args):
+    start_time = time.time()
     #创建目录，防止重名
     save_dir = create_unique_dir(args.save_dir)
     # 初始化SummaryWriter，数据可视化
     writer = SummaryWriter(log_dir=os.path.join(save_dir, 'logs'))
     #创建结果保存的txt文件
+    #保存训练过程
     result_file = os.path.join(save_dir, 'result.txt')
     with open(result_file, 'w') as f:
         f.write("Epoch\tTrain Loss\tTrain Accuracy\tValid Loss\tValid Accuracy\n")
+    #保存超参数
+    params_file = os.path.join(save_dir, 'params.txt')
+    with open(params_file, 'w') as f:
+        f.write(f"Epoch:{args.epochs}\nnum_workers:{args.num_workers}\nbatch_size:{args.batch_size}\nlr:{args.lr}\n")
     # 图片预处理
     val_transform = ImagePreprocessor([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     train_transform = ImageAugmentor([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
@@ -52,10 +61,10 @@ def main(args):
     # 记录分类类别
     num_classes = len(train_dataset.classes)
     # 加载模型
-    Net = models.resnet50(pretrained=True)
+    Net = models.vit_b_16(weights = ViT_B_16_Weights.IMAGENET1K_V1)
     # 修改最后一个全连接层以匹配分类数量
-    num_ftrs = Net.fc.in_features
-    Net.fc = nn.Linear(num_ftrs, num_classes)
+    num_heads = Net.heads.head.in_features
+    Net.heads.head = nn.Linear(num_heads, num_classes)
     Net.to(device)
     # 显示模型结构
     inputs_example, _ = next(iter(train_loader))
@@ -65,15 +74,15 @@ def main(args):
     # 定义优化器
     # 将参数分为两组,不同层用不同学习率
     base_params = []
-    fc_params = []
+    head_params = []
     for name, param in Net.named_parameters():
-        if 'fc' in name:
-            fc_params.append(param)
+        if 'head' in name:
+            head_params.append(param)
         else:
             base_params.append(param)
-    optimizer = optim.Adam([
+    optimizer = optim.AdamW([
         {'params': base_params, 'lr': args.lr},
-        {'params': fc_params, 'lr': args.lr * 10}
+        {'params': head_params, 'lr': args.lr * 10}
     ], weight_decay=1e-4)
     scheduler = CosineAnnealingLR(optimizer,T_max=args.epochs,eta_min=1e-6)
     # 初始化 GradScaler
@@ -88,8 +97,9 @@ def main(args):
         for inputs, labels in train_loader:
             inputs, labels = inputs.to(device), labels.to(device)
             optimizer.zero_grad()
-            outputs = Net(inputs)
-            loss = criterion(outputs, labels)
+            with autocast():
+                outputs = Net(inputs)
+                loss = criterion(outputs, labels)
             scaler.scale(loss).backward()
             scaler.step(optimizer)
             scaler.update()
@@ -107,8 +117,9 @@ def main(args):
         with torch.no_grad():
             for inputs, labels in val_loader:
                 inputs, labels = inputs.to(device), labels.to(device)
-                outputs = Net(inputs)
-                loss = criterion(outputs, labels)
+                with autocast():
+                    outputs = Net(inputs)
+                    loss = criterion(outputs, labels)
                 running_loss_val += loss.item() * inputs.size(0)
                 accuracy_val += (outputs.argmax(1) == labels).sum().item()
             epoch_loss_val = running_loss_val / len(val_dataset)
@@ -125,6 +136,8 @@ def main(args):
             model_save_path = os.path.join(save_dir, "best_model.pth")
             torch.save(Net.state_dict(), model_save_path)
             print(f"Best val Accuracy: {best_val_acc:.4f}%")
+    with open(os.path.join(save_dir, 'params.txt'), 'a') as f:
+        f.write(f"Trainning Time:{time.time() - start_time}\n")
     writer.close()
 
 
@@ -132,9 +145,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir', type=str, default='../垃圾图片库')
     parser.add_argument('--save_dir', type=str, default='./run/train/exp')
-    parser.add_argument('--num_workers', type=int, default=24)
+    parser.add_argument('--num_workers', type=int, default=16)
     parser.add_argument('--epochs', type=int, default=100)
-    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--lr', type=float, default=1e-5)
     parser.add_argument('--label_smoothing', type=float, default=0.0)
     parser.add_argument('--batch_size', type=int, default=256)
     args = parser.parse_args()
